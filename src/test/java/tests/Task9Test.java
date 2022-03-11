90664663a65c4bd8637f71cde7b4fc02f3f468ff
ildar <sunagatov@haulmont.com>
2022-03-11 14:02:15 +0400

    fix Task9Test

--- src/test/java/tests/Task9Test.java
+++ src/test/java/tests/Task9Test.java
@@ -7,7 +7,6 @@ import org.testng.asserts.SoftAssert;
 import page.LiteCartAdminStartPage;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,34 +17,29 @@ public class Task9Test extends TestBase {
         driver.get("http://localhost:8081/litecart/admin/?app=geo_zones&doc=geo_zones");
         LiteCartAdminStartPage.loginToAdmin();
         List<WebElement> rowsList =  driver.findElements(By.className("row"));
-        int rowsNumbers = rowsList.size();
-        List<String> sortedListCountries = new ArrayList<>();
-        WebElement row;
-        for (int i=0; i<rowsNumbers; i++) {
-            row = driver.findElements(By.className("row")).get(i);
-            row.click();
-            row.findElement(By.cssSelector("a")).click();
-            List<WebElement> listRow = driver.findElement(By.className("dataTable")).findElements(By.cssSelector("tr"));
-            listRow.remove(0);
-            listRow.remove(listRow.size()-1);
-            List<WebElement> listOption;
+        List<String> sortedListCountries;
+        int rows = rowsList.size();
+        WebElement rowCurrent;
+        for (int i = 0;  i < rows; ++i) {
+            rowCurrent = driver.findElements(By.className("row")).get(i);
+            rowCurrent.findElement(By.cssSelector("a")).click();
+            List<WebElement> listCountriesWebEl = driver.findElements(By.xpath("//table[@id='table-zones']//td[3]//select[contains(@name,'zones[')]//option[@selected]"));
+
             List<String> listCountries = new ArrayList<>();
-            for (WebElement rowCurrent:listRow){
-                                listOption = rowCurrent.findElements(By.cssSelector("td")).get(2).findElements(By.cssSelector("option"));
-                for (WebElement optionCurrent:listOption) {
-                    if(optionCurrent.isSelected()==true){
-                         listCountries.add(optionCurrent.getText());
-                         break;
-                    }
-                }
+            //listCountries.add("ZZZ");
+            for (WebElement countryCurrent:listCountriesWebEl){
+                listCountries.add(countryCurrent.getText());
             }
-            sortedListCountries = listCountries;
+
+            sortedListCountries = new ArrayList<>(listCountries); //Fixed
             Collections.sort(sortedListCountries);
+
             sa.assertEquals(listCountries, sortedListCountries);
             pressButton("Cancel");
         }
         sa.assertAll();
     }
+
     public void pressButton(String buttonName){
         List<WebElement> listButtons = driver.findElement(By.className("button-set")).findElements(By.cssSelector("button"));
         for(WebElement button:listButtons){
